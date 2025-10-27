5946d73671d89ce6676a45c1706e7d3aa1455bab
Dawid Gębara <gebara.dawid00@gmail.com>
2025-10-27 17:04:24 +0100

    fix sanityLive

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -1,10 +1,14 @@
-// src/sanity/lib/live.ts
-
 import { defineLive } from "next-sanity/live";
 // import your local configured client
-import { client } from "./client";
-
+import { client } from "@/sanity/lib/client";
+// set your viewer token
+const token = process.env.NEXT_PUBLIC_SANITY_WRITE_TOKEN;
+if (!token) {
+	throw new Error("Missing SANITY_API_READ_TOKEN");
+}
 // export the sanityFetch helper and the SanityLive component
 export const { sanityFetch, SanityLive } = defineLive({
 	client,
+	serverToken: token,
+	browserToken: token,
 });
